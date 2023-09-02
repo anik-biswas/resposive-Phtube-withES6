23afac04ec1d796ff080aeeb30abd24ba11c4f9e
anik-biswas <anik.biswas.an@gmail.com>
2023-09-02 09:26:24 -0700

    category item dynamically loaded  dynamically added

--- index.js
+++ index.js
@@ -9,19 +9,34 @@ const handleCategory = async() => {
     });
      
 } ;
+handleCategory();
+
 const handleLoadCategory = async (category_id) => {
     const res = await fetch(` https://openapi.programming-hero.com/api/videos/category/${category_id}`);
     const data = await res.json();
+
     const cardContainer = document.getElementById("card-container");
-    data.data.forEach((videos) => {
+    cardContainer.innerHTML = '';
+        const selectCategory = category_id;
+        console.log(selectCategory)
+    data.data.forEach((videos ) => {
+       
+        if( selectCategory === "1000" || videos.category_id===selectCategory) {
         
+        const  secToHours = (seconds)=> {
+            const hours = Math.floor(seconds / 3600); 
+            const minutes = Math.floor((seconds % 3600) / 60); 
+          
+            return { hours, minutes };
+          };
+          const time = secToHours(videos.others.posted_date);
+
         const div = document.createElement("div");
         div.innerHTML = `  <div class="card w-72 bg-base-100 shadow-xl">
         <div class="relative">
             <figure><img src=${videos?.thumbnail} class=" h-48 w-full bg-slate-400 rounded-xl  " alt="Shoes" /></figure>
-            <h3 class="absolute text-sm text-white bg-slate-600 bottom-5 right-5">Bottom Right</h3>
+            <h3 class="absolute text-sm text-white bg-slate-600 bottom-5 right-5">${time.hours} hours ${time.minutes } minutes</h3>
         </div>
-        
         <div class="card-body py-5 px-0 ">
               <h2 class="card-title p-0">
                 <div class="">
@@ -29,16 +44,17 @@ const handleLoadCategory = async (category_id) => {
                   </div>
                 ${videos?.title}
               </h2>
-          
-          <p class="px-8" >${videos?.authors[0]?.profile_name}  <span  class="badge"><img src=${videos?.authors[0]?.verified?"images/fi_10629607.svg":null } alt="" srcset=""></span></p>
-          
-          <div class="card-actions px-8">
+              <p class="px-8" >${videos?.authors[0]?.profile_name}  <span  class="badge"><img src=${videos?.authors[0]?.verified?"images/fi_10629607.svg":"" } ></span></p>
+              <div class="card-actions px-8">
            <p>${videos?.others?.views} views</p>
           </div>
         </div>
       </div>`;
-      cardContainer.appendChild(div);
+    cardContainer.appendChild(div);
+       
+        }
+       
+        
     });
-    
-   };
-handleCategory();
+};
+handleLoadCategory("1000");
